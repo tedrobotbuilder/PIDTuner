f3fd31543872cef9fde346492e32de2975fa0ccb
tedrobotbuilder <tedrobotbuilder@gmail.com>
2016-04-15 17:39:12 -0400

    test v3

--- Serial-Data-Plotter-PID-Tuning-Thing/DataPlot3/Program.cs
+++ Serial-Data-Plotter-PID-Tuning-Thing/DataPlot3/Program.cs
@@ -8,7 +8,6 @@ namespace DataPlot3
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
         /// The main entry point for the application.b1
         /// </summary>
         [STAThread]
