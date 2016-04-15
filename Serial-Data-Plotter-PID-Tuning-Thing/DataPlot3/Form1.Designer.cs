d415b7f30c3adb21dd82440de125bca169540943
tedrobotbuilder <tedrobotbuilder@gmail.com>
2016-04-15 17:33:36 -0400

    test v3

--- Serial-Data-Plotter-PID-Tuning-Thing/DataPlot3/Form1.Designer.cs
+++ Serial-Data-Plotter-PID-Tuning-Thing/DataPlot3/Form1.Designer.cs
@@ -3,7 +3,6 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
